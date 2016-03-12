0ab18d44425f67b7531a38a651201c56994fa2e7
anurag03 <anuragsinhab@gmail.com>
2016-03-12 11:43:32 +0530

    making some changes

---
+++ Pet.java
@@ -0,0 +1,12 @@
+public class Pet{
+	int number;
+	public void pet(){
+			number = 2;
+}
+	public static void main(String args []){
+		Pet b = new Pet();
+		b.pet();
+		System.out.println(""+ b.number);
+
+}
+}
